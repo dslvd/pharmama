0e48156ff7d2d687d3759659cb3ca9f7f95349b6
rye <barbyperegil05@gmail.com>
2026-08-25 01:19:53 +0800

    merge

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,10 +37,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <body lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth">
       <body className="antialiased bg-background text-foreground">
         <body>{children}</body>
       </body>
-    </body>
+    </html>
   )
 }
