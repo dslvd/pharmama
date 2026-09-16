81173b81ad75b9afc8add2e30f50e4873d69bec2
Nelson Lago III <lagonelson77@gmail.com>
2026-09-16 16:09:53 +0800

    Added guard and userId requirements

--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -7,40 +7,53 @@ import {
   ParseIntPipe,
   Patch,
   Post,
+  UseGuards,
 } from "@nestjs/common";
 import { ProductService } from "./product.service";
-import { Product } from "src/generated/prisma/client";
-import { CreateProductDto, UpdateProductDto } from "./validation";
+import { Product, Role } from "src/generated/prisma/client";
+import { CreateProductDto, UpdateProductDto } from "./product.validation";
+import { AuthGuard } from "@nestjs/passport";
+import { RolesGuard } from "src/auth/guard/roles.guard";
+import { Roles } from "src/auth/decorator/auth.decorator";
+import { CurrentUser } from "src/auth/decorator/current-user.decorator";
+import type { AuthenticatedUser } from "src/auth/types/jwt-payload.type";
 
 @Controller("product")
+@UseGuards(AuthGuard("jwt"), RolesGuard)
 export class ProductController {
   constructor(private prService: ProductService) {}
 
-  @Get(":id")
-  async getPr(@Param("id", ParseIntPipe) id: number): Promise<Product> {
-    return this.prService.getProduct(id);
-  }
-
   @Get()
+  @Roles(Role.STAFF, Role.OWNER, Role.ADMIN)
   async getPrList(): Promise<Product[]> {
     return this.prService.getProductList();
   }
 
   @Post()
-  async createPr(@Body() data: CreateProductDto): Promise<Product> {
-    return this.prService.createProduct(data);
+  @Roles(Role.OWNER, Role.ADMIN)
+  async createPr(
+    @Body() data: CreateProductDto,
+    @CurrentUser() user: AuthenticatedUser,
+  ): Promise<Product> {
+    return this.prService.createProduct(data, user.userId);
   }
 
   @Patch(":id")
+  @Roles(Role.OWNER, Role.ADMIN)
   async updatePr(
     @Param("id", ParseIntPipe) id: number,
     @Body() body: UpdateProductDto,
+    @CurrentUser() user: AuthenticatedUser,
   ): Promise<Product> {
-    return this.prService.updateProduct(id, body);
+    return this.prService.updateProduct(id, body, user.userId);
   }
 
   @Delete(":id")
-  async deletePr(@Param("id", ParseIntPipe) id: number): Promise<Product> {
-    return this.prService.deleteProduct(id);
+  @Roles(Role.OWNER, Role.ADMIN)
+  async deletePr(
+    @Param("id", ParseIntPipe) id: number,
+    @CurrentUser() user: AuthenticatedUser,
+  ): Promise<Product> {
+    return this.prService.deleteProduct(id, user.userId);
   }
 }
